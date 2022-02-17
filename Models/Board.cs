4fc2fe8fe7a8c559625600d57ebb614abd7587c4
Amit Verma <amit.verma@cybergindia.local>
2022-02-17 10:38:23 +0530

    msg: first commit with basic setup

---
+++ Models/Board.cs
@@ -0,0 +1,6 @@
+using System.Collections.Generic;
+public class Board
+{
+    public string name { get; set; }
+    public List<TodoItem> todos { get; set; }
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,7 @@
+using System;
+public class TodoItem{
+    public string? title {get; set;}
+    public bool isDone{get; set;}
+    public DateTime created {get; set;}
+    public DateTime updated {get; set;}
+}
